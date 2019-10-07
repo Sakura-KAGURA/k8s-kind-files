88de01c4b88820f98291bab07a755ef3ff34215f
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-07 17:50:24 +0900

    Create 99_k8s-kind_clearsetup.sh

---
+++ 99_k8s-kind_clearsetup.sh
@@ -0,0 +1,12 @@
+
+
+cd ~/Downloads
+rm -rf ./k8s-kind-files
+git clone https://github.com/Sakura-KAGURA/k8s-kind-files.git
+chmod +x ./k8s-kind-files/*sh
+cp -p ~/Downloads/k8s-kind-files/99_k8s-kind_clearsetup.sh ~/Downloads
+
+
+ls -la  ~/Downloads/k8s-kind-files/
+
+
