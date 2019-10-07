4cbade555fe9546aebdd24eb44d481774da83a84
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-07 17:38:49 +0900

    Update yfsetup.sh

--- yf/yfsetup.sh
+++ yf/yfsetup.sh
@@ -1,4 +1,7 @@
 
 
-##
-wget 
+##  https://itnext.io/starting-local-kubernetes-using-kind-and-docker-c6089acfc1c0
+### https://kubernetes.io/docs/tutorials/stateful-application/mysql-wordpress-persistent-volume/
+wget https://kubernetes.io/examples/application/wordpress/mysql-deployment.yaml
+wget https://kubernetes.io/examples/application/wordpress/wordpress-deployment.yaml
+
