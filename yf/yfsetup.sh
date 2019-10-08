0e38ed2a9f8148826b060168c0623a561fc1c696
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-08 13:44:13 +0900

    Update yfsetup.sh

--- yf/yfsetup.sh
+++ yf/yfsetup.sh
@@ -1,4 +1,10 @@
 
+export KUBECONFIG="$(kind get kubeconfig-path --name="kind-2")"
+kubectl cluster-info
+
+cd ~/Download/
+mkdir yftmp/
+cd yftmp
 
 ##  https://itnext.io/starting-local-kubernetes-using-kind-and-docker-c6089acfc1c0
 ### https://kubernetes.io/docs/tutorials/stateful-application/mysql-wordpress-persistent-volume/
